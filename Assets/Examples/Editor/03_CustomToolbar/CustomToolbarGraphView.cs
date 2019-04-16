3599bc9f8e9a7bc0448927bf88b81cf71d46c425
Antoine Lelievre <antoinel@unity3d.com>
2019-04-16 14:59:57 +0200

    Added upm branch for unity git package manager reference

--- Assets/Examples/Editor/03_CustomToolbar/CustomToolbarGraphView.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine.UIElements;
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
-using GraphProcessor;
-using System;
-
-public class CustomToolbarGraphView : BaseGraphView
-{
-}
