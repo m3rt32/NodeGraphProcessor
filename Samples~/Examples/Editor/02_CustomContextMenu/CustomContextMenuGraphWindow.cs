970c6a00ddbaee63b4873d7fd9acb7b71746ef07
Antoine Lelievre <antoinel@unity3d.com>
2019-05-20 20:29:48 +0200

    Publish version b831733

---
+++ Samples~/Examples/Editor/02_CustomContextMenu/CustomContextMenuGraphWindow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using GraphProcessor;
+
+public class CustomContextMenuGraphWindow : BaseGraphWindow
+{
+
+	[MenuItem("Window/02_CustomContextMenu")]
+	public static BaseGraphWindow Open()
+	{
+		var graphWindow = GetWindow< CustomContextMenuGraphWindow >();
+
+		graphWindow.Show();
+
+		return graphWindow;
+	}
+
+	protected override void InitializeWindow(BaseGraph graph)
+	{
+		titleContent = new GUIContent("Context Menu Graph");
+
+		var graphView = new CustomContextMenuGraphView();
+
+		rootView.Add(graphView);
+
+		graphView.Add(new MiniMapView(graphView));
+
+		// TODO
+		// graphView.AddElement(new NodeSelectorMenu());
+	}
+}
