2b3cff82cea4482b27fa62a81a43b369e33ad478
Antoine Lelievre <antoinel@unity3d.com>
2019-05-20 20:25:57 +0200

    Publish version e677c90

---
+++ Samples~/Examples/Examples/DefaultNodes/Editor/FloatNodeView.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+using GraphProcessor;
+
+[NodeCustomEditor(typeof(FloatNode))]
+public class FloatNodeView : BaseNodeView
+{
+	public override void Enable()
+	{
+		var floatNode = nodeTarget as FloatNode;
+
+		DoubleField floatField = new DoubleField
+		{
+			value = floatNode.output
+		};
+
+		floatField.RegisterValueChangedCallback((v) => {
+			owner.RegisterCompleteObjectUndo("Updated floatNode output");
+			floatNode.output = (float)v.newValue;
+		});
+
+		controlsContainer.Add(floatField);
+	}
+}
